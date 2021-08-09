93ab70a8e668fb282a923f11465fb2c229ef06e8
Dudu-CNU <k0s0a7@gmail.com>
2021-08-10 01:47:35 +0900

    6025-6031 기본변환

---
+++ Code_Up/6025-6031_기본변환.py
@@ -0,0 +1,18 @@
+'''
+10진수를 입력받아 소문자 16진수 출력
+a = input()
+print('%x'% int(a))
+
+10진수를 입력받아 대문자 16진수 출력
+a = input()
+print('%X'% int(a))
+
+입력된 문자를 유니코드 값으로 변환하여 출력
+print(ord(input()))
+
+입력된 10진수를 문자로 출력
+n = int(input())
+print(chr(n))
+'''
+
+
