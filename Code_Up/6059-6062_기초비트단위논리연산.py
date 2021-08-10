12172c70fbb391ccedc4decc557fb1396cdf8b91
Dudu-CNU <k0s0a7@gmail.com>
2021-08-10 17:53:50 +0900

    6059 - 6062 기초 비트단위 논리 연산 완료

---
+++ Code_Up/6059-6062_기초비트단위논리연산.py
@@ -0,0 +1,13 @@
+'''
+bitwise not 연산자 ( ~ ) => 0과 1을 바꿔줌 
+a = int(input())
+print(~a)
+
+bitwise xor 연산자 ( ^ ) => 서로 다를 때 1 
+<주의> 거듭제곱과 헷갈리지 말 것. 
+a, b = map(int,input().split())
+print(a^b)
+'''
+
+
+
