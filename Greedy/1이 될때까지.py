168b7159c8fb7a3ed5bfc5a485243d19069c6762
Dudu-CNU <k0s0a7@gmail.com>
2021-09-01 17:11:20 +0900

    Greedy 나의 풀이 추가

--- Greedy/1이 될때까지.py
+++ Greedy/1이 될때까지.py
@@ -1,4 +1,22 @@
-#교재 풀이 1
+# 나의 풀이.
+N, K = map(int, input().split())
+count = 0
+
+while True:
+    if N == 1:
+        break
+
+    if N >= K and N % K == 0 and K != 1:
+        N /= K 
+        count += 1
+    else:
+        N -= 1
+        count += 1 
+
+print(count)
+
+'''
+교재 풀이 1
 n, k = map(int, input().split())
 result = 0
 
@@ -15,7 +33,6 @@ while n > 1 :
 
 print(result)
 
-'''
 교재 풀이 2
 n, k = map(int, input().split())
 result = 0
@@ -28,8 +45,8 @@ while True :
     if n < k :
         break
 
-    n //= k
     result += 1
+    n //= k
 
 result += (n-1)
 print(result)
