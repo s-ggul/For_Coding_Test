a1578d8971485a993272b406d53a0e1b25096ee3
Dudu-CNU <k0s0a7@gmail.com>
2021-09-02 21:55:01 +0900

    상!하!좌!우!

---
+++ 이것이코딩테스트다/구현/상하좌우.py
@@ -0,0 +1,26 @@
+N = int(input())
+plans = input().split()
+x, y = 1, 1
+
+# L,R,U,D에 따른 이동 방향
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+move_types = ['L','R','U','D']
+
+# 이동계획을 하나씩 확인
+for plan in plans :
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+        
+    if nx < 1 or nx > N or ny < 1 or ny > N :
+        continue
+
+    x = nx
+    y = ny
+    # x, y = nx, ny 와 같이도 적을 수 있음
+
+print(x, y)
