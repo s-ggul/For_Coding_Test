5f223a03956530b711d917ca3d0969b3678ec31a
Dudu-CNU <k0s0a7@gmail.com>
2022-03-11 19:59:50 +0900

    03-04 : Algorithms

--- 이것이코딩테스트다/구현/왕실의나이트.py
+++ 이것이코딩테스트다/구현/왕실의나이트.py
@@ -20,3 +20,21 @@ for step in steps :
 
 print(result)
 '''
+
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0]) - int(ord('a')) + 1)
+
+steps = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, 1), (2, -1)]
+
+result = 0
+for step in steps : 
+    next_row = row + step[0]
+    next_col = column + step[1]
+
+    if next_row < 1 or next_row > 8 or next_col < 1 or next_col > 8 :
+        continue
+
+    result += 1
+
+print(result)
